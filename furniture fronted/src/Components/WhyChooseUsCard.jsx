d024167f95a2a6253c228f3590efc855ece30caf
shrutigoyal04 <goyalshruti016@gmail.com>
2024-08-17 15:56:21 +0530

    add sh file

--- furniture fronted/src/Components/WhyChooseUsCard.jsx
+++ furniture fronted/src/Components/WhyChooseUsCard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Sh from "./Sh";
 
 function WhyChooseUsCard() {
   return (
@@ -6,7 +7,7 @@ function WhyChooseUsCard() {
       <div className="why-choose-section">
         <div className="container">
           <div className="row my-5">
-            <div className="col-6 col-md-6 col-lg-3 mb-4">
+            {/* <div className="col-6 col-md-6 col-lg-3 mb-4">
               <div className="feature">
                 <div className="icon">
                   <img
@@ -21,111 +22,21 @@ function WhyChooseUsCard() {
                   aliquet velit. Aliquam vulputate.
                 </p>
               </div>
-            </div>
-            <div className="col-6 col-md-6 col-lg-3 mb-4">
-              <div className="feature">
-                <div className="icon">
-                  <img src="images/bag.svg" alt="Image" className="imf-fluid" />
-                </div>
-                <h3>Easy to Shop</h3>
-                <p>
-                  Donec vitae odio quis nisl dapibus malesuada. Nullam ac
-                  aliquet velit. Aliquam vulputate.
-                </p>
-              </div>
-            </div>
-            <div className="col-6 col-md-6 col-lg-3 mb-4">
-              <div className="feature">
-                <div className="icon">
-                  <img
-                    src="images/support.svg"
-                    alt="Image"
-                    className="imf-fluid"
-                  />
-                </div>
-                <h3>24/7 Support</h3>
-                <p>
-                  Donec vitae odio quis nisl dapibus malesuada. Nullam ac
-                  aliquet velit. Aliquam vulputate.
-                </p>
-              </div>
-            </div>
-            <div className="col-6 col-md-6 col-lg-3 mb-4">
-              <div className="feature">
-                <div className="icon">
-                  <img
-                    src="images/return.svg"
-                    alt="Image"
-                    className="imf-fluid"
-                  />
-                </div>
-                <h3>Hassle Free Returns</h3>
-                <p>
-                  Donec vitae odio quis nisl dapibus malesuada. Nullam ac
-                  aliquet velit. Aliquam vulputate.
-                </p>
-              </div>
-            </div>
-            <div className="col-6 col-md-6 col-lg-3 mb-4">
-              <div className="feature">
-                <div className="icon">
-                  <img
-                    src="images/truck.svg"
-                    alt="Image"
-                    className="imf-fluid"
-                  />
-                </div>
-                <h3>Fast &amp; Free Shipping</h3>
-                <p>
-                  Donec vitae odio quis nisl dapibus malesuada. Nullam ac
-                  aliquet velit. Aliquam vulputate.
-                </p>
-              </div>
-            </div>
-            <div className="col-6 col-md-6 col-lg-3 mb-4">
-              <div className="feature">
-                <div className="icon">
-                  <img src="images/bag.svg" alt="Image" className="imf-fluid" />
-                </div>
-                <h3>Easy to Shop</h3>
-                <p>
-                  Donec vitae odio quis nisl dapibus malesuada. Nullam ac
-                  aliquet velit. Aliquam vulputate.
-                </p>
-              </div>
-            </div>
-            <div className="col-6 col-md-6 col-lg-3 mb-4">
-              <div className="feature">
-                <div className="icon">
-                  <img
-                    src="images/support.svg"
-                    alt="Image"
-                    className="imf-fluid"
-                  />
-                </div>
-                <h3>24/7 Support</h3>
-                <p>
-                  Donec vitae odio quis nisl dapibus malesuada. Nullam ac
-                  aliquet velit. Aliquam vulputate.
-                </p>
-              </div>
-            </div>
-            <div className="col-6 col-md-6 col-lg-3 mb-4">
-              <div className="feature">
-                <div className="icon">
-                  <img
-                    src="images/return.svg"
-                    alt="Image"
-                    className="imf-fluid"
-                  />
-                </div>
-                <h3>Hassle Free Returns</h3>
-                <p>
-                  Donec vitae odio quis nisl dapibus malesuada. Nullam ac
-                  aliquet velit. Aliquam vulputate.
-                </p>
-              </div>
-            </div>
+            </div> */}
+            <Sh one ={{icon:"images/truck.svg",title:"Fast Free Shipping",description:"Donec vitae odio quis nisl dapibus malesuada. Nullam ac iquet velit. Aliquam vulputate"}}/>
+            <Sh one ={{icon:"images/bag.svg",title:"Easy to Shop",description:"Donec vitae odio quis nisl dapibus malesuada. Nullam ac iquet velit. Aliquam vulputate"}}/>
+            <Sh one ={{icon:"images/support.svg",title:"24/7 Support",description:"Donec vitae odio quis nisl dapibus malesuada. Nullam ac iquet velit. Aliquam vulputate"}}/>
+            <Sh one ={{icon:"images/return.svg",title:"Hassle Free Returns",description:"Donec vitae odio quis nisl dapibus malesuada. Nullam ac iquet velit. Aliquam vulputate"}}/>
+            <Sh one ={{icon:"images/truck.svg",title:"Fast Free Shipping",description:"Donec vitae odio quis nisl dapibus malesuada. Nullam ac iquet velit. Aliquam vulputate"}}/>
+            <Sh one ={{icon:"images/bag.svg",title:"Easy to Shop",description:"Donec vitae odio quis nisl dapibus malesuada. Nullam ac iquet velit. Aliquam vulputate"}}/>
+            <Sh one ={{icon:"images/support.svg",title:"24/7 Support",description:"Donec vitae odio quis nisl dapibus malesuada. Nullam ac iquet velit. Aliquam vulputate"}}/>
+            <Sh one ={{icon:"images/return.svg",title:"Hassle Free Returns",description:"Donec vitae odio quis nisl dapibus malesuada. Nullam ac iquet velit. Aliquam vulputate"}}/>
+           
+            
+           
+            
+            
+          
           </div>
         </div>
       </div>
