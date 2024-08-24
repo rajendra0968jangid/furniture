e62d4c68180192f739b142d125606c7c2b8500bf
rajendra0968jangid <rajendra0968jangid@gmail.com>
2024-08-24 04:33:56 -0700

    mongoose

--- furniture backend/index.js
+++ furniture backend/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config()
 const express = require('express')
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT||8000
 const cors = require("cors")
 const { Contact } = require("./conn.js")
 const bodyParser = require('body-parser')
